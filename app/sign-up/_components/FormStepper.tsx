ce5ea587983f8f01124b861e3589a654b4f8fd58
Darren Leung <leungyatlong1208@gmail.com>
2023-10-31 21:54:21 +0800

    Styled stepper init state

--- app/sign-up/_components/FormStepper.tsx
+++ app/sign-up/_components/FormStepper.tsx
@@ -16,10 +16,31 @@ export default function FormStepper() {
 
   return (
     <Stepper sx={{ mb: "36px" }} connector={null}>
-      {steps.map(({ path, label }) => (
+      {steps.map(({ path, label }, index) => (
         <Step key={path} active={step === path} sx={{ px: 0, mr: "24px" }}>
-          <StepLabel>
-            <Typography variant="h5">{label}</Typography>
+          <StepLabel
+            StepIconComponent={() => (
+              <Box
+                component="span"
+                width="28px"
+                height="28px"
+                borderRadius={99}
+                bgcolor="primary.main"
+                textAlign="center"
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                mr="4px"
+              >
+                <Typography component="span" color="white" variant="h6">
+                  {index + 1}
+                </Typography>
+              </Box>
+            )}
+          >
+            <Typography component="span" variant="h5">
+              {label}
+            </Typography>
           </StepLabel>
         </Step>
       ))}
